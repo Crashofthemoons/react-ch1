c6d05dc82545000a741f34525c057d0d6014e9a3
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-07-12 11:15:59 -0500

    added bootstrap

--- src/KennelCompany.js
+++ src/KennelCompany.js
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 import NavBar from "./nav/NavBar"
 import ApplicationViews from "./ApplicationViews"
 
-// import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 
 export default class KennelCompany extends Component {
