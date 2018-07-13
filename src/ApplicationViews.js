2249a8ebb0647b9cb03c54f2967e8f648895b7b6
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-07-13 09:26:52 -0500

    final

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -3,6 +3,9 @@ import React, { Component } from "react"
 import AnimalList from './AnimalList'
 import LocationList from './LocationList'
 import EmployeeList from './EmployeeList'
+import Animal from "./Animal"
+import Employee from "./Employee"
+import Location from "./Location"
 
 
 export default class ApplicationViews extends Component {
@@ -10,8 +13,17 @@ export default class ApplicationViews extends Component {
         return (
             <React.Fragment>
                 <Route exact path="/" component={LocationList} />
-                <Route path="/animals" component={AnimalList} />
-                <Route path="/employees" component={EmployeeList} />
+                <Route path="/locations/:locationId" render={(props) => {
+                    return <Location location={props.location.state.location} />
+                }} />
+                <Route exact path="/animals" component={AnimalList} />
+                <Route path="/animals/:animalId" render={(props) => {
+                    return <Animal animal={props.location.state.animal} />
+                }} />
+                <Route exact path="/employees" component={EmployeeList} />
+                <Route path="/employees/:employeeId" render={(props) => {
+                    return <Employee employee={props.location.state.employee} />
+                }} />
             </React.Fragment>
         )
     }
